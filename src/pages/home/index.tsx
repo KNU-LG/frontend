b51c4f188270e8f52ed4f9aa1d4d4d25193801fd
seung365 <bdh3659@naver.com>
2024-09-10 10:07:05 +0900

    feat: homepage 구현

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,63 @@
+import React from "react"
+import styled from "@emotion/styled"
+import { useState, useEffect } from "react"
+
+const HomePage = () => {
+  const [currentTime, setCurrentTime] = useState("")
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date()
+      const seoulTime = new Intl.DateTimeFormat("ko-KR", {
+        timeZone: "Asia/Seoul",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      }).format(now)
+      setCurrentTime(seoulTime)
+    }
+
+    updateTime() // 초기 시간 업데이트
+    const timer = setInterval(updateTime, 1000) // 1초마다 시간 업데이트
+
+    return () => clearInterval(timer) // 컴포넌트 언마운트 시 타이머 정리
+  }, [])
+
+  return (
+    <Wrapper>
+      <Icon className="material-icons">settings</Icon>
+
+      <TimeDisplay>{currentTime}</TimeDisplay>
+    </Wrapper>
+  )
+}
+
+export default HomePage
+
+const Icon = styled.i`
+  display: flex;
+  align-items: flex-start;
+  height: auto;
+  margin: 0;
+  padding: 0;
+  font-size: 50px; /* 필요에 따라 조정 */
+  line-height: 1; /* 기본 line-height를 1로 설정 */
+`
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  font-size: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
+`
+
+const TimeDisplay = styled.div`
+  font-size: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
