8f66a75f964f18f2edc9f401b6c1d6742848a146
seung365 <bdh3659@naver.com>
2024-11-06 14:43:15 +0900

    chore: 안쓰는 import 삭제

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,5 @@
-import React from "react"
-import styled from "@emotion/styled"
 import { css } from "@emotion/react"
+import styled from "@emotion/styled"
 
 type Color = "red" | "white" | "black"
 type Size = "small" | "big"
