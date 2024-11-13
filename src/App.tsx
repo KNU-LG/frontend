f27655de9ba247421b3225d7cbfb48536e1513f9
seungbeom <bdh3659@naver.com>
2024-11-13 13:26:06 +0900

    feat: App에 provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { PositionProvider } from "./provider/PositionContext"
 import Routes from "./routes"
+import Providers from "./provider/Providers"
 
 const App = () => {
   const queryClient = new QueryClient()
+
   return (
     <QueryClientProvider client={queryClient}>
       <PositionProvider>
-        <Routes />
+        <Providers>
+          <Routes />
+        </Providers>
       </PositionProvider>
     </QueryClientProvider>
   )
