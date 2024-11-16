af3e1e20c3849b47f33329ac6772d7763c332a0b
beom <bdh3659@naver.com>
2024-11-17 04:55:07 +0900

    feat: protectedroute 수정

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,14 +1,13 @@
 import { useEffect } from "react"
 import { Outlet, useNavigate } from "react-router-dom"
-import { getDynamicPath, RouterPath } from "./path"
+import { getDynamicPath } from "./path"
 
 export const ProtectedRoute = () => {
   const accessToken = localStorage.getItem("accessToken")
   const navigate = useNavigate()
 
   useEffect(() => {
-    const currentPath = window.location.pathname
-    if (!accessToken && currentPath !== `${RouterPath.login}`) {
+    if (!accessToken) {
       alert("로그인 후 이용해주세요.")
       navigate(getDynamicPath.login())
     }
