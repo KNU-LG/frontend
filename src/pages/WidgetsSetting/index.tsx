5fe439c9c5d3755f3afe4a3a88c030ff22ba7d73
beom <bdh3659@naver.com>
2024-11-24 11:10:38 +0900

    feat: widget setting 페이지에 뒤로가기 버튼 추가

--- src/pages/WidgetsSetting/index.tsx
+++ src/pages/WidgetsSetting/index.tsx
@@ -1,10 +1,22 @@
 import styled from "@emotion/styled"
+import { ArrowBack } from "@mui/icons-material"
+import { useNavigate } from "react-router-dom"
+import { RouterPath } from "../../routes/path"
 import HomeUI from "../Home/components/HomeUI"
 import { WidgetEditor } from "./components/WidgetEditor"
 
 const WidgetsSetting = () => {
+  const navigate = useNavigate()
+
+  const handleBack = () => {
+    navigate(RouterPath.settings)
+  }
+
   return (
     <Wrapper>
+      <IconWrapper onClick={handleBack}>
+        <ArrowBack fontSize="inherit" />
+      </IconWrapper>
       <HomeUI />
       <WidgetEditor />
     </Wrapper>
@@ -16,9 +28,20 @@ export default WidgetsSetting
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   height: 100vh;
   padding: 0 20px;
   box-sizing: border-box;
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  flex-direction: column;
+  gap: 5px;
+  font-size: 40px;
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+  top: 10px;
+  left: 10px;
 `
