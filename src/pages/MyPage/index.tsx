2349e97fd7410d107211a71f2a9c8f028325d355
go-minjung <bmj7474@knu.ac.kr>
2024-11-26 20:56:32 +0900

    feat: 마이페이지 임시 구현

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,7 +1,60 @@
 import React from "react"
+import styled from "@emotion/styled"
+import { Profile } from "../../types"
+import { useGetProfile } from "../../api/useGetProfile"
+import CustomButton from "../../components/Button"
 
 const MyPage = () => {
-  return <div>MyPage</div>
+  const  {data, status} = useGetProfile()
+
+  const 
+
+  return (
+    <Container>
+      <CustumForm>
+        <Title>Profile</Title>
+
+
+        <CustomButton color="red" size="small">
+          Change Password
+        </CustomButton>
+        <CustomButton color="black" size="small">
+          Logout
+        </CustomButton>
+      </CustumForm>
+    </Container>
+  )
 }
 
 export default MyPage
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-color: #f2f2f2;
+`
+
+const CustumForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  height: 500px;
+  padding: 60px 10px;
+  border-radius: 15px;
+  background: #fff;
+  box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.1);
+`
+
+const Title = styled.h2`
+  font-size: 24px;
+  font-weight: 600;
+  color: #616161;
+`
+
+const Name = styled.h2`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`
