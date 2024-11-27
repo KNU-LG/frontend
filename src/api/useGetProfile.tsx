2602a7c6f7ca56ba497978d2210fba0d525b2064
go-minjung <bmj7474@knu.ac.kr>
2024-11-27 20:11:19 +0900

    feat: useGetProfile 수정

--- src/api/useGetProfile.tsx
+++ src/api/useGetProfile.tsx
@@ -1,28 +1,25 @@
 import { useSuspenseQuery } from "@tanstack/react-query"
-import { fetchInstanceWithToken } from "./instance"
-import { isAxiosError } from "axios"
 import { APIResponse, ProfileResponse } from "../types"
+import { fetchInstanceWithToken } from "./instance"
 import { jwtDecode } from "jwt-decode"
 
-const getProfile = async (userId: string): Promise<APIResponse<ProfileResponse[]>>  => {
-    const response = await fetchInstanceWithToken().get("/user/:user-id", {
-        params: {
-            userId: userId,
-        },
-    })
-    return response.data
+const getProfile = async (userId: string): Promise<APIResponse<ProfileResponse>> => {
+  const response = await fetchInstanceWithToken().get("/user/:user-id", {
+    params: {
+      userId: userId,
+    },
+  })
+  const token = userId
+  const decoded = jwtDecode(token)
+  return response.data
 }
 
-const token = 
-const decoded = jwtDecode(token)
-
 export const useGetProfile = (userId: string) => {
-    const { data } = useSuspenseQuery<APIResponse<ProfileResponse[]>, APIResponse<null>>({
-        queryFn: () => getProfile(userId),
-        queryKey: ["schedule", userId],
-        staleTime: 0,
-    })
+  const { data } = useSuspenseQuery<APIResponse<ProfileResponse>, APIResponse<null>>({
+    queryFn: () => getProfile(userId),
+    queryKey: ["profile", userId],
+    staleTime: 0,
+  })
 
-    return { data, status }
-  }
-
+  return { data }
+}
