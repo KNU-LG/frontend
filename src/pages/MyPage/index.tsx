64816a1a62df570e8967fc3cf757daee307f8ef4
go-minjung <bmj7474@knu.ac.kr>
2024-11-29 15:01:25 +0900

    feat: mypage 구현

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,5 +1,8 @@
 import styled from "@emotion/styled"
 import { Suspense } from "react"
+import { ArrowBack } from "@mui/icons-material"
+import { useNavigate } from "react-router-dom"
+import { RouterPath } from "../../routes/path"
 import { ErrorBoundary } from "react-error-boundary"
 import { useGetProfile } from "../../api/useGetProfile"
 import CustomButton from "../../components/Button"
@@ -8,17 +11,38 @@ const MyPageContext = () => {
   const token = localStorage.getItem("accessToken")
   const { data } = useGetProfile(token!)
 
+  const navigate = useNavigate()
+  const handleBack = () => {
+    navigate(RouterPath.settings)
+  }
+
   return (
     <Container>
+      <BackIconWrapper onClick={handleBack}>
+        <ArrowBack fontSize="inherit" />
+      </BackIconWrapper>
       <CustumForm>
         <Title>Profile</Title>
-
-        <CustomButton color="red" size="small">
-          Change Password
-        </CustomButton>
-        <CustomButton color="black" size="small">
-          Logout
-        </CustomButton>
+        <Avatar />
+        <UserInfo>
+          <Name>{data?.data.name}</Name>
+          <UserDetail>
+            <Label>ID</Label>
+            <Value>{data?.data.loginId}</Value>
+          </UserDetail>
+          <UserDetail>
+            <Label>Email</Label>
+            <Value>{data?.data.email}</Value>
+          </UserDetail>
+        </UserInfo>
+        <ButtonWrapper>
+          <CustomButton color="red" size="small">
+            Change Password
+          </CustomButton>
+          <CustomButton color="black" size="small">
+            Logout
+          </CustomButton>
+        </ButtonWrapper>
       </CustumForm>
     </Container>
   )
@@ -40,16 +64,29 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
+  height: 100vh;
   background-color: #f2f2f2;
+`
+
+const BackIconWrapper = styled.div`
+  position: absolute;
+  flex-direction: column;
+  gap: 5px;
+  font-size: 40px;
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+  top: 10px;
+  left: 10px;
 `
 
 const CustumForm = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 500px;
-  height: 500px;
-  padding: 60px 10px;
+  height: auto;
+  padding: 50px 10px;
   border-radius: 15px;
   background: #fff;
   box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.1);
@@ -59,10 +96,49 @@ const Title = styled.h2`
   font-size: 24px;
   font-weight: 600;
   color: #616161;
+  margin-bottom: 50px;
+`
+
+const Avatar = styled.div`
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  background-color: #f2f2f2;
+  margin-bottom: 20px;
+`
+
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 50px;
 `
 
 const Name = styled.h2`
   font-size: 18px;
   font-weight: bold;
+  color: #5a5a5a;
   margin-bottom: 20px;
+`
+
+const UserDetail = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-bottom: 10px;
+`
+
+const Label = styled.span`
+  font-size: 14px;
+  font-weight: 600;
+  color: #5a5a5a;
+`
+
+const Value = styled.span`
+  font-size: 14px;
+  color: #5a5a5a;
+`
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `
