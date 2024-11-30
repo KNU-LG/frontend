e16e9c5cd8e8bdd57663d338852e4e57e61fbf47
beom <bdh3659@naver.com>
2024-11-30 19:38:33 +0900

    feat: setting 화면에 들어가는 setting button 추가

---
+++ src/pages/Settings/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled"
+
+export const SettingButton = styled.button`
+  background-color: var(--color-white);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: 600;
+  max-width: 200px;
+  width: 100%;
+  height: 55px;
+  color: (--color-dark-gray);
+  transition: all 0.3s;
+  &:hover {
+    background-color: #e2e2e2;
+  }
+`
