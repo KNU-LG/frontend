333f9d700ccfa5b0e5e9fba3c6f00238ead0c505
beom <bdh3659@naver.com>
2024-11-30 20:59:27 +0900

    feat: postclock 추가

---
+++ src/api/clock/usePostClock.tsx
@@ -0,0 +1,33 @@
+import { useMutation } from "@tanstack/react-query"
+
+import { APIResponse, ClockResponse, ClockSend } from "../../types"
+
+import { isAxiosError } from "axios"
+
+import { fetchInstanceWithToken } from "../instance"
+
+const postClock = async (clockInfo: ClockSend): Promise<APIResponse<ClockResponse>> => {
+  const response = await fetchInstanceWithToken().post("/widget/clock/add", clockInfo)
+  console.log("전송 데이터:", clockInfo) // 추가된 로그
+
+  return response.data
+}
+
+export const usePostClock = () => {
+  const { mutate, status } = useMutation<APIResponse<ClockResponse>, APIResponse<null>, ClockSend>({
+    mutationFn: (clockInfo: ClockSend) => postClock(clockInfo),
+    onError: (error) => {
+      if (isAxiosError(error)) {
+        const errorMessage = error.response?.data?.message || "알 수 없는 오류가 발생했습니다."
+        alert(`시계 추가에 실패하였습니다. 이유 : ${errorMessage}`)
+      } else {
+        alert("시계 추가 중 오류가 발생했습니다.")
+      }
+    },
+    onSuccess: () => {
+      alert("시계가 성공적으로 추가되었습니다.")
+    },
+  })
+
+  return { mutate, status }
+}
