8ebf31b3fa4bbe5c8fb265a6dd40441933d7845a
beom <bdh3659@naver.com>
2024-12-02 20:45:20 +0900

    design: BackgroundUI도 기본 이미지 추가

--- src/pages/Settings/components/BackgroundUI/index.tsx
+++ src/pages/Settings/components/BackgroundUI/index.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled"
+import defaultBackground from "../../../../assets/defaultBackground.svg"
 
 const BackgroundUI = styled.div<{ backgroundImage: string }>`
   border-radius: 70px;
@@ -11,7 +12,7 @@ const BackgroundUI = styled.div<{ backgroundImage: string }>`
   align-items: center;
 
   background-image: ${({ backgroundImage }) =>
-    backgroundImage ? `url(${backgroundImage})` : "none"};
+    backgroundImage ? `url(${backgroundImage})` : `url(${defaultBackground})`};
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
