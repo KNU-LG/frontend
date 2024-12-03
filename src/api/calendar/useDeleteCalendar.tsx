624f73aaf41ab0fb176c423701bef1b3450a592b
beom <bdh3659@naver.com>
2024-12-03 19:25:47 +0900

    refactor: removeFromLocalStorage hook 분리

--- src/api/calendar/useDeleteCalendar.tsx
+++ src/api/calendar/useDeleteCalendar.tsx
@@ -2,54 +2,14 @@ import { useMutation } from "@tanstack/react-query"
 import { isAxiosError } from "axios"
 import { APIResponse } from "../../types"
 import { fetchInstanceWithToken } from "../instance"
-
-interface Widget {
-  type: string
-  size: string
-  key: number
-}
-
-interface WidgetPosition {
-  position: {
-    x: number
-    y: number
-  }
-  widgetName: string
-}
-
-const deleteCalendar = async (calendarId: number): Promise<APIResponse<null>> => {
-  const response = await fetchInstanceWithToken().delete(`/widget/calendar/${calendarId}`)
-  return response.data
-}
-
-const removeFromLocalStorage = (calendarId: number) => {
-  // 위젯 포지션 업데이트
-  const savedPositions = localStorage.getItem("widgetPositions")
-  if (savedPositions) {
-    const positions: Record<string, WidgetPosition> = JSON.parse(savedPositions)
-    const updatedPositions = Object.entries(positions).reduce(
-      (acc, [key, value]) => {
-        // Calendar-{calendarId} 형식의 키를 가진 항목 제외
-        if (!key.includes(`-${calendarId}-Calendar`)) {
-          acc[key] = value
-        }
-        return acc
-      },
-      {} as Record<string, WidgetPosition>,
-    )
-    localStorage.setItem("widgetPositions", JSON.stringify(updatedPositions))
-  }
-
-  // 위젯 목록 업데이트
-  const savedWidgets = localStorage.getItem("widgets")
-  if (savedWidgets) {
-    const widgets: Widget[] = JSON.parse(savedWidgets)
-    const updatedWidgets = widgets.filter((widget) => widget.key !== calendarId)
-    localStorage.setItem("widgets", JSON.stringify(updatedWidgets))
-  }
-}
+import removeFromLocalStorage from "./hooks/removeLocalStorage"
 
 export const useDeleteCalendar = (calendarId: number) => {
+  const deleteCalendar = async (calendarId: number): Promise<APIResponse<null>> => {
+    const response = await fetchInstanceWithToken().delete(`/widget/calendar/${calendarId}`)
+    return response.data
+  }
+
   const { mutate, status } = useMutation<APIResponse<null>, APIResponse<null>, number>({
     mutationFn: (id: number) => deleteCalendar(id),
     onError: (error) => {
