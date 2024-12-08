c4d918ecf144f71438599758ca8425379f532c85
go-minjung <bmj7474@knu.ac.kr>
2024-12-08 18:24:37 +0900

    design: 날씨 위젯 수정

--- src/components/WidgetsUI/WeatherUI.tsx
+++ src/components/WidgetsUI/WeatherUI.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled"
+import weather from "../../assets/weather.svg"
 
 type WeatherUIProp = {
   size: "L" | "M" | "S"
@@ -6,19 +7,15 @@ type WeatherUIProp = {
 
 export const WeatherUI = ({ size, ...props }: WeatherUIProp) => {
   const dimensions = {
-    L: { width: 210, height: 140 },
-    M: { width: 150, height: 100 },
-    S: { width: 120, height: 80 },
+    L: { width: 240, height: 130 },
+    M: { width: 180, height: 100 },
+    S: { width: 140, height: 80 },
   }
 
   const widgetWidth = dimensions[size].width
   const widgetHeight = dimensions[size].height
 
-  return (
-    <Wrapper widgetHeight={widgetHeight} widgetWidth={widgetWidth} {...props}>
-      <p>Weather widget UI</p>
-    </Wrapper>
-  )
+  return <Wrapper widgetHeight={widgetHeight} widgetWidth={widgetWidth} {...props}></Wrapper>
 }
 
 const Wrapper = styled.div<{ widgetHeight: number; widgetWidth: number }>`
@@ -27,10 +24,9 @@ const Wrapper = styled.div<{ widgetHeight: number; widgetWidth: number }>`
   align-items: center;
   width: ${({ widgetWidth }) => widgetWidth}px;
   height: ${({ widgetHeight }) => widgetHeight}px;
-  background-color: #f0f0f0;
-  font-size: 20px;
-  border: 1px solid gray;
+  background-image: url(${weather});
+  background-size: contain;
   border-radius: 15px;
-  padding: 5px;
+  box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.1);
   cursor: pointer;
 `
